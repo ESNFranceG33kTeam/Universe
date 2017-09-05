0d4ae0314de451d9a45e7375f40ceeff3ea02fe1
Rémy Raes <raes.remy@gmail.com>
2017-09-05 18:34:27 +0300

    Adding "news count" system

--- assets/js/notification.js
+++ assets/js/notification.js
@@ -8,11 +8,29 @@ ipc.on('exit-notification' , function(){
 function add_notification_on_site(url) {
 	// check if the notification span exists
 	let component = document.getElementById(url_to_css_id(url));
+	let child_nodes = component.getElementsByTagName('SPAN');
 	
 	// create it if not
+	if(child_nodes.length === 0) {
+		let spa = document.createElement("SPAN");
+		spa.innerText = 1;
+		spa.className = 'new_item';
+		component.appendChild(spa);
+	}
+	
 	// +1
+	else {
+		let spa = child_nodes[0];
+		spa.innerText = parseInt(spa.innerText) + 1;
+	}
+	
 }
 
 function remove_notification_from_site(url) {
 	// delete the notification span
+	let component = document.getElementById(url_to_css_id(url));
+	let spa = component.getElementsByTagName("SPAN");
+	
+	if(spa.length != 0)
+		component.removeChild(spa[0]);
 }
