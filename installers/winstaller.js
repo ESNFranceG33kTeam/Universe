2b37d6ada8e4d2f691160067a46f1b03c6a55166
Alystrasz <raes.remy@gmail.com>
2018-01-02 17:03:50 +0100

    Added version support for windows installer compilation

--- installers/winstaller.js
+++ installers/winstaller.js
@@ -1,17 +1,33 @@
 var electronInstaller = require('electron-winstaller');
+const fs = require('fs');
 var distDir = '/universe-winstaller';
+let packageJson = undefined;
 
-console.log('Beginning installer creation...')
-resultPromise = electronInstaller.createWindowsInstaller({
-    appDirectory: './Universe-win32-ia32',
-    outputDirectory: distDir,
-    iconUrl: 'https://raw.githubusercontent.com/ESNFranceG33kTeam/Universe/master/assets/img/icons/star.ico',
-    setupIcon: './assets/img/icons/installerStar.ico',
-    loadingGif: './assets/img/loading.gif',
-    authors: 'ESN France - G33k Team',
-    exe: 'universe.exe',
-    setupExe: 'UniverseInstaller-' + '0.4.1' + '.exe',
-    noMsi: true
-  });
+fs.readFile('package.json', 'utf8',
+function (err, data) {
 
-resultPromise.then(() => console.log("Installer created in " + distDir), (e) => console.log(`No dice: ${e.message}`));
+    packageJson = JSON.parse(data);
+    let version = packageJson["version"];
+
+    console.log('Beginning installer creation for version ' + version)
+
+    if (err) {
+        console.error('Failed to read package.json');
+        return ;
+    }
+
+
+    resultPromise = electronInstaller.createWindowsInstaller({
+      appDirectory: './Universe-win32-ia32',
+      outputDirectory: distDir,
+      iconUrl: 'https://raw.githubusercontent.com/ESNFranceG33kTeam/Universe/master/assets/img/icons/star.ico',
+      setupIcon: './assets/img/icons/installerStar.ico',
+      loadingGif: './assets/img/loading.gif',
+      authors: 'ESN France - G33k Team',
+      exe: 'universe.exe',
+      setupExe: 'UniverseInstaller-' +  + '.exe',
+      noMsi: true
+    });
+
+    resultPromise.then(() => console.log("Installer created in " + distDir), (e) => console.log(`No dice: ${e.message}`));
+});
